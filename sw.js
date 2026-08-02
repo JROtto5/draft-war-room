ff679a9ce37836d8147fcc0c4dd8d6853a1e77c6
JROtto5 <otto@phoneburner.com>
2026-08-02 15:32:31 -0400

    Release v7.0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "war-room-v7";
+const CACHE = "war-room-v8";
 const CORE = ["./", "./index.html", "./styles.css", "./data.js", "./engine.js", "./app.js", "./manifest.json", "./icon.svg", "./icon-192.png", "./icon-512.png", "./fonts/Sora-400.woff2", "./fonts/JetBrainsMono-400.woff2"];
 
 self.addEventListener("install", e => {
